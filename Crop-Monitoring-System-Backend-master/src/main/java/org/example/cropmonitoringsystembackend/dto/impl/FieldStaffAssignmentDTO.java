62afe79ee60a0877eba605f07a90e0942fa5338a
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-12-05 21:19:24 +0530

    update

--- Crop-Monitoring-System-Backend-master/src/main/java/org/example/cropmonitoringsystembackend/dto/impl/FieldStaffAssignmentDTO.java
+++ Crop-Monitoring-System-Backend-master/src/main/java/org/example/cropmonitoringsystembackend/dto/impl/FieldStaffAssignmentDTO.java
@@ -1,9 +1,10 @@
 package org.example.cropmonitoringsystembackend.dto.impl;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class FieldStaffAssignmentDTO {
