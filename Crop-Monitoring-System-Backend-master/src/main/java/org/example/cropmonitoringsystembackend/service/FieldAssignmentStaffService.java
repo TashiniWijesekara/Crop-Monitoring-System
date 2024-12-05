12bd4c811aebb98a4e40bff653135fcc6d056ebc
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-12-05 21:27:48 +0530

    update

--- Crop-Monitoring-System-Backend-master/src/main/java/org/example/cropmonitoringsystembackend/service/FieldAssignmentStaffService.java
+++ Crop-Monitoring-System-Backend-master/src/main/java/org/example/cropmonitoringsystembackend/service/FieldAssignmentStaffService.java
@@ -2,6 +2,8 @@ package org.example.cropmonitoringsystembackend.service;
 
 import org.example.cropmonitoringsystembackend.dto.impl.FieldStaffAssignmentDTO;
 
+import java.util.List;
+
 public interface FieldAssignmentStaffService {
     void saveAssignment(FieldStaffAssignmentDTO fieldStaffAssignmentDTO);
     List<> getAllFieldStaffAssignments();
